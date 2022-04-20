9bc8ea50280403b9cada2c842cff924f26ba4a33
Martijn Koppejan <456795@student.fontys.nl>
2022-04-20 17:21:31 +0200

    Integrate frontend with backend

--- MissionSystem/MissionSystem.Main/GameService.cs
+++ MissionSystem/MissionSystem.Main/GameService.cs
@@ -5,13 +5,23 @@ namespace MissionSystem.Main;
 public class GameService : IGameService
 {
     private IServiceProvider serviceProvider;
+    
+    private Dictionary<string, IBaseGame> games = new();
+
     public GameService(IServiceProvider provider)
     {
         serviceProvider = provider;
     }
-    public IBaseGame GetGame(string type)
+    public IBaseGame GetGame(string key)
     {
+
         // if (type == "ctf") return GameFactory.GetBaseGame(serviceProvider);
-        return GameFactory.GetBaseGame(serviceProvider);
+        if (!games.ContainsKey(key))
+        {
+            games[key] = GameFactory.GetBaseGame(serviceProvider);
+            games[key].Setup();
+        }
+
+        return games[key];
     }
 }
