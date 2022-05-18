22cd1d47782c72711c951725e0534ae64fc490d2
Raqbit <raqbit@outlook.com>
2022-05-18 08:47:17 +0200

    Add ArenaService

---
+++ MissionSystem/MissionSystem.Interface/Services/IArenaService.cs
@@ -0,0 +1,42 @@
+using MissionSystem.Data.Models;
+using MissionSystem.Util;
+
+namespace MissionSystem.Interface.Services;
+
+/// <summary>
+/// Service which can be used to manage arenas
+/// </summary>
+public interface IArenaService : ISubscribableResource<Arena>
+{
+    /// <summary>
+    /// Gets a list of all arenas currently known to the system
+    /// </summary>
+    /// <returns>All arenas currently known to the system</returns>
+    public Task<List<Arena>> GetArenasAsync();
+    
+    /// <summary>
+    /// Adds a new arena to the system
+    /// </summary>
+    /// <param name="arena">Arena to add</param>
+    public Task AddArenaAsync(Arena arena);
+
+    /// <summary>
+    /// Removes a arena from the system
+    /// </summary>
+    /// <param name="arena">Arena to remove</param>
+    public Task DeleteArenaAsync(Arena arena);
+
+    /// <summary>
+    /// Gets a arena from the system
+    /// </summary>
+    /// <param name="id">The ID of the arena to retrieve</param>
+    /// <returns></returns>
+    public Task<Arena?> FindArenaAsync(int id);
+
+    /// <summary>
+    /// Update a arena in the system
+    /// </summary>
+    /// <param name="arena">Arena to update</param>
+    /// <returns></returns>
+    public Task UpdateArenaAsync(Arena arena);
+}
---
+++ MissionSystem/MissionSystem.Main/Arenas/ArenaService.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using MissionSystem.Data;
+using MissionSystem.Data.Models;
+using MissionSystem.Interface.Services;
+using MissionSystem.Util;
+
+namespace MissionSystem.Main.Arenas;
+
+public class ArenaService : SubscribableResource<Arena>, IArenaService
+{
+    public async Task<List<Arena>> GetArenasAsync()
+    {
+        await using var db = new DataStore();
+        return await db.Arenas.ToListAsync();
+    }
+
+
+    public async Task<Arena?> FindArenaAsync(int id)
+    {
+        await using var db = new DataStore();
+        var arena = await db.Arenas.FindAsync(id);
+
+        return arena;
+    }
+
+    public async Task AddArenaAsync(Arena arena)
+    {
+        await using var db = new DataStore();
+
+        await db.Arenas.AddAsync(arena);
+        await db.SaveChangesAsync();
+
+        OnAdded(arena);
+    }
+
+    public async Task DeleteArenaAsync(Arena arena)
+    {
+        await using var db = new DataStore();
+
+        db.Arenas.Remove(arena);
+        await db.SaveChangesAsync();
+
+        OnDeleted(arena);
+    }
+
+    public async Task UpdateArenaAsync(Arena arena)
+    {
+        await using var db = new DataStore();
+
+        db.Update(arena);
+        await db.SaveChangesAsync();
+    }
+}
